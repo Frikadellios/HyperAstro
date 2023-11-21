4c3d514a006fa628a1147e249af852040b740931
Hrihorii Ilin <devopsick@pm.me>
2023-11-21 19:07:55 +0000

    add components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
-import { defineConfig } from 'astro/config';
-import alpine from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
+import alpine from "@astrojs/alpinejs";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    i18n: {
+      defaultLocale: "uk",
+      locales: ["uk", "en", "ru"]
+    }
+  },
+  prefetch: {
+    prefetchAll: true
+  },
   integrations: [alpine(), react(), tailwind()]
 });
